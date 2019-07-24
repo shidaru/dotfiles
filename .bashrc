10ae8eedb1a55eab145513d75559802d13a7e110
shidaru <shidaru.pub@gmail.com>
2019-07-24 23:52:26 +0900

    add telephone-line

--- .bashrc
+++ .bashrc
@@ -210,3 +210,7 @@ shopt -s cdspell
 shopt -s dotglob
 shopt -s extglob
 shopt -s globstar
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -226,6 +226,18 @@ PATHS is you want to include."
   (global-disable-mouse-mode)
   )
 
+;; mode-line
+(use-package telephone-line
+  :config
+  (setq telephone-line-primary-left-separator 'telephone-line-cubed-left
+	telephone-line-secondary-left-separator 'telephone-line-cubed-hollow-left
+	telephone-line-primary-right-separator 'telephone-line-cubed-right
+	telephone-line-secondary-right-separator 'telephone-line-cubed-hollow-right)
+  (setq telephone-line-height 24
+	telephone-line-evil-use-short-tag t)
+  (telephone-line-mode 1)
+  )
+
 ;; カッコの色付け
 (use-package rainbow-delimiters
   :ensure t
@@ -429,6 +441,15 @@ Otherwise indent whole buffer."
       (indent-region (region-beginning) (region-end))
     (all-indent)))
 (global-set-key (kbd "C-M-\\") 'electric-indent)
+
+(use-package twittering-mode
+  :config
+  ;; 簡単ログインの設定
+  (setq twittering-allow-insecure-server-cert t)
+  (setq twittering-use-master-password t)
+  ;; (setq twittering-private-info-file "~/.emacs.d/twittering-mode.gpg")
+  ;; (setq twittering-status-format "%i @%s %S %p: n %T  [%@]%r %R %f%Ln ------------------------------" )
+  )
 
 ;; インターフェース設定終了
 
@@ -772,3 +793,25 @@ Otherwise indent whole buffer."
 (provide 'init)
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(avy-migemo-function-names
+   (quote
+    (swiper--add-overlays-migemo
+     (swiper--re-builder :around swiper--re-builder-migemo-around)
+     (ivy--regex :around ivy--regex-migemo-around)
+     (ivy--regex-ignore-order :around ivy--regex-ignore-order-migemo-around)
+     (ivy--regex-plus :around ivy--regex-plus-migemo-around)
+     ivy--highlight-default-migemo ivy-occur-revert-buffer-migemo ivy-occur-press-migemo avy-migemo-goto-char avy-migemo-goto-char-2 avy-migemo-goto-char-in-line avy-migemo-goto-char-timer avy-migemo-goto-subword-1 avy-migemo-goto-word-1 avy-migemo-isearch avy-migemo-org-goto-heading-timer avy-migemo--overlay-at avy-migemo--overlay-at-full)))
+ '(package-selected-packages
+   (quote
+    (telephone-line twittering-mode meghanada yatex yasnippet-snippets yaml-mode web-mode undohist undo-tree swiper spinner rainbow-delimiters python-mode py-yapf org-bullets open-junk-file nlinum neotree mwim mozc markdown-mode magit latex-math-preview ivy-yasnippet ivy-rich htmlize ht highlight-indentation helm-swoop helm-migemo google-translate go-eldoc flycheck find-file-in-project expand-region exec-path-from-shell epc dumb-jump dockerfile-mode disable-mouse diminish dash-functional company-go comment-dwim-2 avy-migemo anzu))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
