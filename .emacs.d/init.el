8c2fe0986d5ddd4e2e30eafff9798224794876dd
shidaru <shidaru.pub@gmail.com>
2022-05-07 00:18:55 +0900

    add emacs-mozc config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -188,8 +188,26 @@
 	    (lambda() (set-cursor-color "green")))
   (add-hook 'input-method-inactivate-hook
 	    (lambda() (set-cursor-color "orange")))
-  (global-set-key (kbd "C-j") 'toggle-input-method)
-  (define-key minibuffer-local-map (kbd "C-j") 'toggle-input-method)
+  ;; (global-set-key (kbd "C-j") 'toggle-input-method)
+  ;; (define-key minibuffer-local-map (kbd "C-j") 'toggle-input-method)
+  (global-set-key [henkan]
+		  (lambda () (interactive)
+		    (when (null current-input-method) (toggle-input-method))))
+  (global-set-key [muhenkan]
+		  (lambda () (interactive)
+		    (inactivate-input-method)))
+  ; 全角半角キーと無変換キーのキーイベントを横取りする
+  (defadvice mozc-handle-event (around intercept-keys (event))
+    "Intercept keys muhenkan and zenkaku-hankaku, before passing keys
+to mozc-server (which the function mozc-handle-event does), to
+properly disable mozc-mode."
+    (if (member event (list 'zenkaku-hankaku 'muhenkan))
+	(progn
+	  (mozc-clean-up-session)
+	  (toggle-input-method))
+      (progn ;(message "%s" event) ;debug
+	ad-do-it)))
+  (ad-activate 'mozc-handle-event)
   )
 ;; 改行文字の文字列表現
 (set 'eol-mnemonic-dos "(CRLF)")
